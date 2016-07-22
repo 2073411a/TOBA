8091645afd0615244c150fe29e82bf06ebf8181d
Duncan Adamson <2073411a@student.gla.ac.uk>
2016-07-22 10:15:30 +0100

    Create Pixel.java

---
+++ Pixel.java
@@ -0,0 +1,10 @@
+public class Pixel{
+  public long x;
+  public long y;
+  public String colourCode;
+  public Pixel(long x, long y, String colourCode){
+    this.x = x;
+    this.y = y;
+    this.colourCode = colourCode;
+  }
+}
